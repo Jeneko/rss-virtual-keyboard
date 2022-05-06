a33f042381c3f797b86e7dba9f1c89d3b6aa6f5a
Jeneko <jenekomail@gmail.com>
2022-05-06 14:31:34 +0300

    init: start virtual-keyboard task

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const EslintWebpackPlugin = require('eslint-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    watch: true,
+    mode: 'production',
+    entry: './src/index.js',
+    devtool: 'source-map',
+    output: {
+        filename: 'script.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new EslintWebpackPlugin(),
+        new MiniCssExtractPlugin(),
+    ]
+}
