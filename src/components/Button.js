f4ef78df00c9fe6973ec361ff35947e74d68c277
Jeneko <jenekomail@gmail.com>
2022-05-06 15:40:49 +0300

    refactor: rewrite Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,26 +1,42 @@
-export default class Button {
-  constructor({
-    key, color = 'default', size = '1u', alt = '',
-  }) {
-    this.key = key;
-    this.color = color;
-    this.size = size;
-    this.alt = alt;
+export default function createButton(data) {
+  const buttonColor = data.color || 'default';
+  const buttonSize = data.size || '1u';
+  const buttonKey = (typeof data.key === 'object') ? data.key.en : data.key;
+
+  const button = document.createElement('button');
+
+  button.classList.add('btn');
+  button.classList.add(`btn--${buttonColor}`);
+  button.classList.add(`btn--${buttonSize}`);
+
+  button.setAttribute('tabindex', '-1');
+  button.value = data.code;
+
+  // If key is a system key
+  if (data.system) {
+    button.insertAdjacentHTML('beforeend', `<span class="btn__syskey">${buttonKey}</span>`);
   }
 
-  getElement() {
-    const btn = document.createElement('button');
-    btn.classList.add('btn');
-    btn.classList.add(`btn--${this.color}`);
-    btn.classList.add(`btn--${this.size}`);
-    btn.value = this.key;
-    if (this.alt) {
-      const alt = document.createElement('span');
-      alt.className = 'btn__alt';
-      alt.innerHTML = this.alt;
-      btn.append(alt);
-    }
-    btn.append(this.key);
-    return btn;
+  // If key is a standard key and has alt Key
+  // ATTENTION: btn__altkey must stands prior to btn__stdkey for correct css style
+  if (data.altkey) {
+    button.insertAdjacentHTML('beforeend', `<span class="btn__altkey">${data.altkey.en}</span>`);
   }
+
+  // If key is a standard key
+  if (!data.system) {
+    button.insertAdjacentHTML('beforeend', `<span class="btn__stdkey">${buttonKey}</span>`);
+  }
+
+  // If key has an icon
+  if (data.icon) {
+    button.innerHTML = `<i class="material-icons">${data.icon}</i>`;
+  }
+
+  // If key has led indicator
+  if (data.hasLed) {
+    button.insertAdjacentHTML('beforeend', '<div class="btn__led"></div>');
+  }
+
+  return button;
 }
